a1bba4a8957591f15ef6f06f5f240c5f585aec83
Yurii Zaliskyi <yuriizaliskii@gmail.com>
2023-11-30 12:56:08 +0200

    fix pagination

--- src/features/Pagination/Pagination.jsx
+++ src/features/Pagination/Pagination.jsx
@@ -6,18 +6,32 @@ import {
   onNavigatePrev,
 } from "../../features/List/listSlice";
 import PaginationButton from "../../ui/PaginationButton/PaginationButton";
+import { useState } from "react";
 
 function Pagination({ pages, currentPage, totalPages }) {
+  const [pageNumberLimit, setPageNumberLimit] = useState(5);
+  const [maxPageNumberLimit, setMaxPageNumberLimit] = useState(5);
+  const [minPageNumberLimit, setMinPageNumberLimit] = useState(0);
   const dispatch = useDispatch();
 
   function navigatePrev() {
     if (currentPage !== 1) {
       dispatch(onNavigatePrev());
+      if ((currentPage - 1) % pageNumberLimit == 0) {
+        if (currentPage === totalPages) return;
+        setMaxPageNumberLimit(maxPageNumberLimit - pageNumberLimit);
+        setMinPageNumberLimit(minPageNumberLimit - pageNumberLimit);
+      }
     }
   }
   function navigateNext() {
     if (currentPage !== totalPages) {
       dispatch(onNavigateNext());
+    }
+    if (currentPage + 1 > maxPageNumberLimit) {
+      if (currentPage === totalPages) return;
+      setMaxPageNumberLimit(maxPageNumberLimit + pageNumberLimit);
+      setMinPageNumberLimit(minPageNumberLimit + pageNumberLimit);
     }
   }
   function handleCurrentPage(p) {
@@ -37,17 +51,23 @@ function Pagination({ pages, currentPage, totalPages }) {
           Prev
         </PaginationButton>
 
-        {pages.map((p) => (
-          <span
-            key={p}
-            onClick={() => handleCurrentPage.call(null, p)}
-            className={`${
-              p === currentPage ? "bg-indigo-500 text-white" : ""
-            } relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1  ring-gray-300 hover:bg-indigo-500 hover:text-white focus:z-20 focus:outline-offset-0 cursor-pointer transition-all`}
-          >
-            {p}
-          </span>
-        ))}
+        {pages.map((p) => {
+          if (p < maxPageNumberLimit + 1 && p > minPageNumberLimit) {
+            return (
+              <span
+                key={p}
+                onClick={() => handleCurrentPage.call(null, p)}
+                className={`${
+                  p === currentPage ? "bg-indigo-500 text-white" : ""
+                } relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1  ring-gray-300 hover:bg-indigo-500 hover:text-white focus:z-20 focus:outline-offset-0 cursor-pointer transition-all`}
+              >
+                {p}
+              </span>
+            );
+          } else {
+            return null;
+          }
+        })}
         <PaginationButton onClick={navigateNext} direction="ml">
           Next
           <BsChevronRight size="1.5em" className="inline" />
